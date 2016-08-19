24c6b9688cc25dc9ed40dcf87bc4e14df1db8870
Liam <liam.hinzman@gmail.com>
2016-08-18 23:32:19 -0400

    Attempted to fix dynamic square resizing. Add change grid size button

--- js/script.js
+++ js/script.js
@@ -6,29 +6,61 @@ $(document).ready(function(){
   var borderThickness = 1;
 
   var clearButton = document.getElementById('clearButton');
-  console.log(clearButton)
-  for(col = 1; col <= gridHeight; col++){
-    $('#grid').append("<div class = 'container' id = 'col" + col + "'></div>");
-    for(row = 1; row <= gridWidth; row++){
-      $('#col' + col).append("<div></div>");
-    };
-  };
+  var changeGridSize = document.getElementById('changeGridSize');
 
-  var style = $("<style> .container > div{ display: inline-block; height: " + (gridSize / (gridHeight + (borderThickness * 2))) + "px; width: " + (gridSize / (gridWidth + (borderThickness * 2))) + "px; border: " + borderThickness + "px solid black; } #grid{ display: block; margin: 50px auto; height: " + (gridSize + (borderThickness * 2)) + "px; width: " + (gridSize + (borderThickness * 2)) + "px; } .container{ display: block; margin: 0px auto; height: " + (gridSize / (gridHeight + (borderThickness * 2))) + "px; width: " + (gridSize + (borderThickness * 2)) + "px; } </style>");
-  $('html > head').append(style);
 
-  $('div > div > div').mouseenter(function(){
-    $(this).css('background-color', 'black');
-  });
+
+  function createGrid(){
+    for(col = 1; col <= gridHeight; col++){
+      $('#grid').append("<div class = 'container' id = 'col" + col + "'></div>");
+      for(row = 1; row <= gridWidth; row++){
+        $('#col' + col).append("<div></div>");
+      }
+    }
+  }
+
+  function destroyGrid(){
+    for(col = 1; col <= gridHeight; col++){
+      $('.container').remove();
+    }
+  }
 
   function clear(){
     $('div > div > div').css('background-color', 'white');
-  };
+  }
+
+  function sizeChange(){
+    var answer = prompt('Pick a new grid size', 'i.e 30x40');
+    gridHeight = answer.substr(0, answer.indexOf('x'));
+    gridWidth = answer.substr((answer.indexOf('x')+1), answer.length);
+    destroyGrid();
+    createGrid();
+    applyCss();
+  }
+
+  function applyCss(){
+    $('style').remove();
+    var style = $("<style> .container > div{ display: inline-block; height: " + (gridSize / (gridHeight + (borderThickness * 2))) + "px; width: " + (gridSize / (gridWidth + (borderThickness * 2))) + "px; border: " + borderThickness + "px solid black; } #grid{ display: block; margin: 50px auto; height: " + (gridSize + (borderThickness * 2)) + "px; width: " + (gridSize + (borderThickness * 2)) + "px; } .container{ display: block; margin: 0px auto; height: " + (gridSize / (gridHeight + (borderThickness * 2))) + "px; width: " + (gridSize + (borderThickness * 2)) + "px; } </style>");
+    $('html > head').append(style);
+  }
+
 
   $(clearButton).click(function(){
-    $('div > div > div').css('background-color', 'white');
-  });
+    clear();
+  })
 
-});
+  $(changeGridSize).click(function(){
+    sizeChange();
+  })
 
-//.append("<div class = 'col"+ col +" row"+row +"'></div>");
+
+
+  createGrid();
+
+  applyCss();
+
+  $('.container > div').mouseenter(function(){
+    $(this).css('background-color', 'black');
+  })
+
+})
