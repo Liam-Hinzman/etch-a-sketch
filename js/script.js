f0066b860d4c48e810a7a8254ee334b2379296e1
Liam <liam.hinzman@gmail.com>
2016-08-18 21:34:43 -0400

    Added etch-a-sketch funtions. When mouse enters a square the square turns black. Added dynamic grid resizing, the amount of squares large the grid is can be changed from within the code, the square sizes will change to remain in the same area. Added clear button, which clears the grid area.

--- js/script.js
+++ js/script.js
@@ -1,16 +1,34 @@
 $(document).ready(function(){
-  length = 25;
-  gridHeight = 16;
-  gridWidth = 16;
-  borderThickness = 1;
+
+  var gridSize = 800;
+  var gridHeight = 16;
+  var gridWidth = 16;
+  var borderThickness = 1;
+
+  var clearButton = document.getElementById('clearButton');
+  console.log(clearButton)
   for(col = 1; col <= gridHeight; col++){
     $('#grid').append("<div class = 'container' id = 'col" + col + "'></div>");
     for(row = 1; row <= gridWidth; row++){
       $('#col' + col).append("<div></div>");
     };
   };
-  var style = $("<style> .container > div{ display: inline-block; height: " + length + "px; width: " + length + "px; border: " + borderThickness + "px solid black; } .grid{ display: block; margin: 100px auto; height: 100%; width: 100%; } .container{ display: block; margin: 0px auto; height: " + length + "px; width: " + ((length + (borderThickness * 2)) * gridWidth) + "px; } </style>");
+
+  var style = $("<style> .container > div{ display: inline-block; height: " + (gridSize / (gridHeight + (borderThickness * 2))) + "px; width: " + (gridSize / (gridWidth + (borderThickness * 2))) + "px; border: " + borderThickness + "px solid black; } #grid{ display: block; margin: 50px auto; height: " + (gridSize + (borderThickness * 2)) + "px; width: " + (gridSize + (borderThickness * 2)) + "px; } .container{ display: block; margin: 0px auto; height: " + (gridSize / (gridHeight + (borderThickness * 2))) + "px; width: " + (gridSize + (borderThickness * 2)) + "px; } </style>");
   $('html > head').append(style);
+
+  $('div > div > div').mouseenter(function(){
+    $(this).css('background-color', 'black');
+  });
+
+  function clear(){
+    $('div > div > div').css('background-color', 'white');
+  };
+
+  $(clearButton).click(function(){
+    $('div > div > div').css('background-color', 'white');
+  });
+
 });
 
 //.append("<div class = 'col"+ col +" row"+row +"'></div>");
